daad5ed039f3b4bc6fc8a1d34b2c5b899620201b
Syaugi SA <syaugiamar9@gmail.com>
2022-06-09 20:56:17 +0700

    add .emv

--- server/README.md
+++
@@ -1,9 +0,0 @@
-# Aplikasi Konsultasi Kesehatan
-
-## Oleh : Rifai (E4)
-
-## Tools dan Freamwork :
-
-- ReactJs
-- ExpressJs
-- MongoDb
--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const dotenv = require("dotenv");
 
 const indexRouter = require("./routes/index-router");
 const usersRouter = require("./routes/users-router");
 const authRouter = require("./routes/auth-router");
+const { connect } = require("mongoose");
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+connect(`${process.dotenv.Database_Url}`);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
